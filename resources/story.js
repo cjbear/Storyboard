fb6b923181636ed3206ec43b5fadb355071a1e1c
Chris Skyfarm <30752468+cjbear@users.noreply.github.com>
2018-11-01 10:43:48 -0700

    First commit of BackPocketStoryboard

---
+++ resources/story.js
@@ -0,0 +1,199 @@
+var story = {
+"pages": [
+{
+"image": "backpockethome.png",
+"image2x": "backpockethome@2x.png",
+"width": 375,
+"height": 957,
+"title": "BackPocketHome",
+"links": [
+{
+  "rect": [
+    16,
+    156,
+    179,
+    319
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    196,
+    156,
+    359,
+    319
+   ],
+   "page": 3
+  }
+,{
+  "rect": [
+    16,
+    336,
+    359,
+    679
+   ],
+   "page": 2
+  }
+,{
+  "rect": [
+    16,
+    695,
+    179,
+    858
+   ],
+   "page": 5
+  }
+,{
+  "rect": [
+    196,
+    695,
+    359,
+    858
+   ],
+   "page": 4
+  }
+,{
+  "rect": [
+    259,
+    877,
+    303,
+    921
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    72,
+    877,
+    116,
+    921
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "self_assess.png",
+"image2x": "self_assess@2x.png",
+"width": 375,
+"height": 1144,
+"title": "Self Assess",
+"links": [
+{
+  "rect": [
+    259,
+    1064,
+    303,
+    1108
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "assignments_&_tasks.png",
+"image2x": "assignments_&_tasks@2x.png",
+"width": 375,
+"height": 1144,
+"title": "Assignments & Tasks",
+"links": [
+{
+  "rect": [
+    259,
+    1064,
+    303,
+    1108
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "goals.png",
+"image2x": "goals@2x.png",
+"width": 375,
+"height": 1144,
+"title": "Goals",
+"links": [
+{
+  "rect": [
+    259,
+    1064,
+    303,
+    1108
+   ],
+   "page": 1
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+,{
+  "rect": [
+    72,
+    1064,
+    116,
+    1108
+   ],
+   "page": 0
+  }
+]}
+,{
+"image": "reflections_&_stories.png",
+"image2x": "reflections_&_stories@2x.png",
+"width": 375,
+"height": 1391,
+"title": "Reflections & Stories",
+"links": [
+]}
+,{
+"image": "portfolio.png",
+"image2x": "portfolio@2x.png",
+"width": 375,
+"height": 812,
+"title": "Portfolio",
+"links": [
+]}
+   ]
+,"resolutions": [2],
+"title": "Prototype",
+"highlightLinks": false
+}
